6cc064e4cfc5119e73df3444f84d99ed410ea53c
Artur_Barilo <bariloartur@gmail.com>
2022-07-23 13:41:53 +0300

    Finished....or not

---
+++ burger.js
@@ -0,0 +1,71 @@
+
+function toggleBurger() {
+    var burger = document.getElementById('burger');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
+
+function toggleBurgerLanguages() {
+    var burger = document.getElementById('burgerLanguages');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
+
+function toggleBurgercurrencys() {
+    var burger = document.getElementById('burgerCurrencys');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
+
+function toggleBurgerSize() {
+    var burger = document.getElementById('burgerFilterSize');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
+
+function toggleBurgerColor() {
+    var burger = document.getElementById('burgerFilterColor');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
+
+function toggleBurgerPrice() {
+    var burger = document.getElementById('burgerFilterPrice');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
+
+function toggleBurgerSortBy() {
+    var burger = document.getElementById('burgerFilterSortBy');
+
+    if (!burger.style.display || burger.style.display == 'none') {
+        burger.style.display = 'block';
+    } else {
+        burger.style.display = 'none';
+    }
+}
